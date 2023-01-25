14015dad3d0a5f2d051b4da86d7454ba73e39161
mejidza <clackz2k16@gmail.com>
2023-01-25 22:34:06 +0300

    added register and authorize pages

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Register } from './pages/register/Register';
+import { Authorization } from './pages/authorization/Authorization';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/registration",
+    element: <Register/>,
+  },
+  {
+    path: "/authorization",
+    element: <Authorization/>,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
